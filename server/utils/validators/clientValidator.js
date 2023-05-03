8452a9775a9e6b56cccf906c12d4c2e46db1b7e4
Your Name <is_sam97@live.fr>
2023-05-04 00:04:16 +0100

    ok

---
+++ server/utils/validators/clientValidator.js
@@ -0,0 +1,64 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const {
+  isUniqueEmail,
+  isArrayOfProjects,
+} = require("../../middlewares/customCheck");
+
+// Finds the validation errors in this request and wraps them in an object with handy functions
+
+exports.getClientByIdValidator = [
+  check("id").isMongoId().withMessage("invalid client ID format"),
+  validatorMiddleware,
+];
+
+exports.createClientValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Client name required")
+    .isLength({ min: 3 })
+    .withMessage("Too short Client name")
+    .isLength({ max: 32 })
+    .withMessage("Too long Client name"),
+  check("email")
+    .notEmpty()
+    .withMessage("Email address required")
+    .isEmail()
+    .withMessage("This field should be a valid email")
+    .custom(isUniqueEmail),
+  check("phone").notEmpty().withMessage("Phone number required"),
+  check("projects")
+    .isArray({ max: 30 })
+    .withMessage("projects must be an array")
+    .custom(isArrayOfProjects)
+    .optional(),
+  validatorMiddleware,
+];
+
+exports.updateClientValidator = [
+  check("id").isMongoId().withMessage("invalid client ID format"),
+  validatorMiddleware,
+];
+
+exports.deleteClientValidator = [
+  check("id").isMongoId().withMessage("invalid client ID format"),
+  validatorMiddleware,
+];
+
+exports.addProjectToClient = [
+  check("projectName")
+    .notEmpty()
+    .withMessage("Project name required")
+    .isLength({ min: 3 })
+    .withMessage("Too short Project  name")
+    .isLength({ max: 32 })
+    .withMessage("Too long Project name"),
+  check("clientName")
+    .notEmpty()
+    .withMessage("Project name required")
+    .isLength({ min: 3 })
+    .withMessage("Too short Project  name")
+    .isLength({ max: 32 })
+    .withMessage("Too long Project name"),
+  validatorMiddleware,
+];
---
+++ server/utils/validators/projectValidator.js
@@ -0,0 +1,92 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const {
+  isArrayOfEmployees,
+  isArrayOfTasks,
+  isValidManager,
+  isValidClient,
+  isUniqueName,
+} = require("../../middlewares/customCheck");
+
+exports.getProjectByIdValidator = [
+  check("id").isMongoId().withMessage("invalid Project ID format"),
+  validatorMiddleware,
+];
+
+exports.createProjectValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Project must have a name ")
+    .isLength({ min: 3 })
+    .withMessage("Too short project name")
+    .isLength({ max: 32 })
+    .withMessage("Too long project name")
+    .custom(isUniqueName),
+  check("client")
+    .notEmpty()
+    .withMessage("Project must belong to a client")
+    .isMongoId()
+    .withMessage("Invalid client ID format")
+    .custom(isValidClient),
+  check("productionManager")
+    .notEmpty()
+    .withMessage("Project must have a productionManager")
+    .isMongoId()
+    .withMessage("Invalid productionManager ID format")
+    .custom(isValidManager)
+    .optional(),
+  check("team")
+    .isArray({ max: 30 })
+    .withMessage("Team members are required and must be an array"),
+  check("chainOfTasks")
+    .isArray({ max: 30 })
+    .withMessage("ChainOfTasks must be an array")
+    .custom(isArrayOfTasks)
+    .optional(),
+  validatorMiddleware,
+];
+
+exports.updateProjectValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Project must have a name ")
+    .isLength({ min: 3 })
+    .withMessage("Too short project name")
+    .isLength({ max: 32 })
+    .withMessage("Too long project name")
+    .custom(isUniqueName),
+  check("client")
+    .notEmpty()
+    .withMessage("Project must belong to a client")
+    .isMongoId()
+    .withMessage("Invalid client ID format")
+    .custom(isValidClient),
+  check("productionManager")
+    .notEmpty()
+    .withMessage("Project must have a productionManager")
+    .isMongoId()
+    .withMessage("Invalid productionManager ID format")
+    .custom(isValidManager),
+  check("team")
+    .isArray({ max: 30 })
+    .withMessage("Team members are required and must be an array")
+    .custom(isArrayOfEmployees)
+    .withMessage("At least one ID has an invalid format"),
+  check("chainOfTasks")
+    .isArray({ max: 30 })
+    .withMessage("ChainOfTasks must be an array")
+    .custom(isArrayOfTasks)
+    .optional(),
+  validatorMiddleware,
+];
+
+exports.addProjectToClientValidator = [
+  check("clientID").isMongoId().withMessage("Invalid client ID format"),
+  check("projectID").isMongoId().withMessage("Invalid project ID format"),
+  validatorMiddleware,
+];
+
+exports.deleteProjectValidator = [
+  check("id").isMongoId().withMessage("invalid project ID format"),
+  validatorMiddleware,
+];
